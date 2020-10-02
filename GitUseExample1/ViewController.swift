afc64db18390a2fc61b3a9449a1eee95f7c897ac
Joel Vallejo <joelcvallejo02@gmail.com>
2020-10-02 13:57:49 -0500

    Initial Commit

---
+++ GitUseExample1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample1
+//
+//  Created by Vallejo, Joel C on 10/2/20.
+//  Copyright © 2020 Vallejo, Joel C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

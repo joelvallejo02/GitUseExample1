5eae26596b8fe90561d2eb9c043290d6556f1713
Joel Vallejo <joelcvallejo02@gmail.com>
2020-10-06 13:39:37 -0500

    first commit to branch

---
+++ GitUseExample1/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample1
+//
+//  Created by Vallejo, Joel C on 10/6/20.
+//  Copyright © 2020 Vallejo, Joel C. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary {
+    
+}
